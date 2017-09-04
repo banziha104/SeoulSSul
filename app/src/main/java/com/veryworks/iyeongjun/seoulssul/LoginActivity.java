fd79e2dc18bd8187421e59298af34e9df74d713d
iyeongjun <they6687@naver.com>
2017-09-04 15:22:42 +0900

    Add Login

--- app/src/main/java/com/veryworks/iyeongjun/seoulssul/LoginActivity.java
+++ app/src/main/java/com/veryworks/iyeongjun/seoulssul/LoginActivity.java
@@ -4,15 +4,21 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @BindView(R.id.videoView)
-    VideoView videoView;
+    @BindView(R.id.videoView) VideoView videoView;
+    @BindView(R.id.btnFacebook) ImageButton btnFacebook;
+    @BindView(R.id.btnNextTime) ImageButton btnNextTime;
+    @BindView(R.id.btnKakao) ImageButton btnKakao;
+    @BindView(R.id.imgLogo) ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.login);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.login);
         videoView.setVideoURI(uri);
         videoView.start();
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -29,5 +35,17 @@ public class LoginActivity extends AppCompatActivity {
                 mediaPlayer.setLooping(true);
             }
         });
+    }
+    @OnClick(R.id.btnFacebook)
+    private void goMainWithFacebook(){
+
+    }
+    @OnClick(R.id.btnKakao)
+    private void goMainWithKakao(){
+
+    }
+    @OnClick(R.id.btnNextTime)
+    private void goMainWithNextTime(){
+
     }
 }
