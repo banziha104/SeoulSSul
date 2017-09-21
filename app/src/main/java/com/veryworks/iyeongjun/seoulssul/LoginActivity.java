c572d4db6297ad8822ac0d9621d5daf5c5c8b514
iyeongjun <they6687@naver.com>
2017-09-21 09:24:35 +0900

    Add FacebookLogin

--- app/src/main/java/com/veryworks/iyeongjun/seoulssul/LoginActivity.java
+++ app/src/main/java/com/veryworks/iyeongjun/seoulssul/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -21,17 +22,17 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.veryworks.iyeongjun.seoulssul.Domain.Const; 
+import com.veryworks.iyeongjun.seoulssul.Domain.Const;
 import com.veryworks.iyeongjun.seoulssul.Util.PermissionControl;
 
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import butterknife.BindView;
@@ -42,26 +43,37 @@ import static com.veryworks.iyeongjun.seoulssul.Domain.UserData.userInstance;
 import static com.veryworks.iyeongjun.seoulssul.Util.PermissionControl.checkVersion;
 
 
-public class LoginActivity extends AppCompatActivity implements PermissionControl.CallBack{
+public class LoginActivity extends AppCompatActivity implements PermissionControl.CallBack {
 
-    @BindView(R.id.videoView) VideoView videoView;
-    @BindView(R.id.btnFacebook) ImageButton btnFacebook;
-    @BindView(R.id.btnNextTime) ImageButton btnNextTime;
-    @BindView(R.id.btnKakao) ImageButton btnKakao;
-    @BindView(R.id.imgLogo) ImageView imgLogo;
+    @BindView(R.id.videoView)
+    VideoView videoView;
+    @BindView(R.id.btnFacebook)
+    ImageButton btnFacebook;
+    @BindView(R.id.btnNextTime)
+    ImageButton btnNextTime;
+    @BindView(R.id.btnKakao)
+    ImageButton btnKakao;
+    @BindView(R.id.imgLogo)
+    ImageView imgLogo;
+    @BindView(R.id.facebookLoginButton)
+    LoginButton loginButton;
     CallbackManager callbackManager;
+    AccessToken accessToken;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkVersion(this);
-        }else {
+        } else {
             init();
         }
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -73,62 +85,60 @@ public class LoginActivity extends AppCompatActivity implements PermissionContro
         ButterKnife.bind(this);
         mAuth = FirebaseAuth.getInstance();
         setVideoView();
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        accessToken = AccessToken.getCurrentAccessToken();
+        callbackManager = CallbackManager.Factory.create();
     }
+
     /**
      * 페이스북 로그인 버튼
      */
     @OnClick(R.id.btnFacebook)
     public void goMainWithFacebook() {
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this,
-                Arrays.asList("Facebook", "email"));
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-
+       facebookLoginButton();
+        loginButton.performClick();
+    }
+    public void facebookLoginButton(){
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onSuccess(final LoginResult result) {
-
-                GraphRequest request;
-                request = GraphRequest.newMeRequest(result.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
-                    public void onCompleted(JSONObject user, GraphResponse response) {
-                        if (response.getError() != null) {
-
-                        } else {
-                            Log.i("FacebookOK", "user: " + user.toString());
-                            Log.i("FacebookToken", "AccessToken: " + result.getAccessToken().getToken());
-                            setResult(RESULT_OK);
-
-                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(i);
-                            finish();
-                        }
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v("result",object.toString());
+                        Log.v("result",accessToken.toString());
                     }
                 });
-                Bundle parameters = new Bundle();
-                parameters.putString("FackbookBundle", "id,name,email,gender,birthday");
-                request.setParameters(parameters);
-                request.executeAsync();
-            }
 
-            @Override
-            public void onError(FacebookException error) {
-                Log.e("FalseBookError", "Error: " + error);
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
             }
 
             @Override
             public void onCancel() {
-                Log.e("FalseBookCancel", "Cancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LoginErr",error.toString());
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     /**
      *
      */
     @OnClick(R.id.btnKakao)
-    public void goMainWithKakao(){
+    public void goMainWithKakao() {
         Toast.makeText(this, "카카오 로그인은 릴리즈 버전에서만 가능", Toast.LENGTH_SHORT).show();
     }
 
@@ -136,17 +146,17 @@ public class LoginActivity extends AppCompatActivity implements PermissionContro
      *
      */
     @OnClick(R.id.btnNextTime)
-    public void goMainWithNextTime(){
-        guestLogin(Const.Guest.GUEST_EMAIL,Const.Guest.GUEST_PASSWORD);
+    public void goMainWithNextTime() {
+        guestLogin(Const.Guest.GUEST_EMAIL, Const.Guest.GUEST_PASSWORD);
 
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     /**
      * Setting Video
      */
-    private void setVideoView(){
+    private void setVideoView() {
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.login);
         videoView.setVideoURI(uri);
         videoView.start();
@@ -157,7 +167,8 @@ public class LoginActivity extends AppCompatActivity implements PermissionContro
             }
         });
     }
-    private void FirebaseAuthrizater(){
+
+    private void FirebaseAuthrizater() {
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -172,13 +183,14 @@ public class LoginActivity extends AppCompatActivity implements PermissionContro
             }
         };
     }
-    private void guestLogin(String email,String password){
+
+    private void guestLogin(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this,"게스트로 시작합니다",
+                            Toast.makeText(LoginActivity.this, "게스트로 시작합니다",
                                     Toast.LENGTH_SHORT).show();
                             userInstance.setName("GUEST");
                             userInstance.setImage_url("noImage");
